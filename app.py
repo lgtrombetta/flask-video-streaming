484616f895f7e53e508862e489303d4e6b2360a6
Leonardo G. Trombetta <lgtrombetta@gmx.com>
2020-04-19 17:25:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ class DSLR:
         self.p2 = Popen(split("ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0"), stdin=self.p1.stdout, stdout=DEVNULL, stderr=DEVNULL)
 
     def pid(self):
-	return self.p1.pid
-
+        return self.p1.pid
 
 cam = DSLR()
 
